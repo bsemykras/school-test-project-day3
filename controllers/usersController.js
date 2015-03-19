27a67bd04c0f99b36305233377472d7f2438b7be
lebronua5 <lebronua5@gmail.com>
2015-03-19 10:32:33 +0200

    fix bug -> replace json on js module

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,7 +12,7 @@ module.exports = {
             response.statusCode = 200;
             try{
                 response.write(renderer.render('users.ect', {
-                    allUsers: users.items
+                    allUsers: users.Users
                 })) ;
                 next();
             }catch(e){
@@ -30,17 +30,20 @@ module.exports = {
         request.on("end", function () {
 
             var personinfo = JSON.parse(path);
+
             if (validator.isLength(personinfo["name"], 1, 255) && validator.isEmail(personinfo["e-mail"]) &&
                 validator.isNumeric(personinfo["age"]) && validator.isLength(personinfo["description"], 1, 255)) {
 
-                users.items.push({
+                users.Users.push({
                     name: personinfo["name"],
                     email: personinfo["e-mail"],
                     description: personinfo["description"],
                     age: personinfo["age"]
                 });
-                response.write("Save new object")
-                next;
+                response.write("Save new object");
+
+                next();
+
 
             } else{
                 response.statusCode=404;
